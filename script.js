177e69a4bad1261073e5380b7f38c752dbd32899
Valerian Ha <ha.valerian@gmail.com>
2025-03-05 12:52:25 -0500

    Disable buttons when game ends

--- script.js
+++ script.js
@@ -11,21 +11,21 @@ function getComputerChoice() {
     return comp_choice;
 }
 
-function getHumanChoice() {
-    let human_choice = "";
-    let userInput = false;
-    while (userInput === false) {
-        human_choice = prompt("What's your choice?");
-        human_choice = human_choice.toLowerCase();
-        human_choice = human_choice.charAt(0).toUpperCase() + human_choice.slice(1); 
-        if (human_choice == "Rock" || human_choice == "Scissors" || human_choice == "Paper") {
-            userInput = true;
-            return human_choice;
-        } else {
-            alert("Your input is not valid. Please try again.");
-        }
-    }
-}
+// function getHumanChoice() {
+//     let human_choice = "";
+//     let userInput = false;
+//     while (userInput === false) {
+//         human_choice = prompt("What's your choice?");
+//         human_choice = human_choice.toLowerCase();
+//         human_choice = human_choice.charAt(0).toUpperCase() + human_choice.slice(1); 
+//         if (human_choice == "Rock" || human_choice == "Scissors" || human_choice == "Paper") {
+//             userInput = true;
+//             return human_choice;
+//         } else {
+//             alert("Your input is not valid. Please try again.");
+//         }
+//     }
+// }
 
 function playRound(humanChoice, computerChoice) {
     let foundWinner = false;
@@ -37,7 +37,6 @@ function playRound(humanChoice, computerChoice) {
             return;
         }
     }
-
     let rockWeight = 3;
     let scissorsWeight = 2;
     let paperWeight = 1;
@@ -79,14 +78,32 @@ function playRound(humanChoice, computerChoice) {
         } else {
             display.textContent = "You have LOST the game";
         }
-        
     } 
 }
 
-function disableButtons() {
-    choice.removeEventListener("click", event);
+function startGame() {
+    rockBtn.addEventListener("click", function chooseRock() {
+        if (humanScore + computerScore == 5) {
+            rockBtn.removeEventListener("click", chooseRock);
+        } else {
+            playRound("Rock", getComputerChoice());
+        }  
+    });
+    paperBtn.addEventListener("click", function choosePaper() { 
+        if (humanScore + computerScore == 5) {
+            paperBtn.removeEventListener("click", choosePaper);
+        } else {
+            playRound("Paper", getComputerChoice());
+        }
+    });
+    scissorsBtn.addEventListener("click", function chooseScissors() {  
+        if (humanScore + computerScore == 5) {
+            scissorsBtn.removeEventListener("click", chooseScissors);
+        } else {
+            playRound("Scissors", getComputerChoice());
+        }
+    });
 }
-       
 
 let humanScore = 0;
 let computerScore = 0;
@@ -94,19 +111,7 @@ const playerScore = document.querySelector("#player-score");
 const compScore = document.querySelector("#computer-score");
 const result = document.querySelector("#result-box");
 const display = document.querySelector("#result-display");
-let choice = document.querySelector("#button-box");
-
-choice.addEventListener("click", (event) => {
-    let target = event.target;
-    switch (target.id) {
-        case "rock-btn":
-            playRound("Rock", getComputerChoice());
-            break;
-        case "paper-btn":
-            playRound("Paper", getComputerChoice());
-            break;
-        case "scissors-btn":
-            playRound("Scissors", getComputerChoice());
-            break;
-    }
-});   
+let rockBtn = document.querySelector("#rock-btn");
+let paperBtn = document.querySelector("#paper-btn");
+let scissorsBtn = document.querySelector("#scissors-btn");
+startGame();
